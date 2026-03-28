d613108a4795430444acc183cad0750345ac4230
ujjwalsaini45 <ujjwalsaini00@gmail.com>
2026-03-28 13:23:51 +0530

    he

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly. hiii . everyone 1111
+            Save and see your changes instantly. hiii . everyone hii
           </li>
         </ol>
 
